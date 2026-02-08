1cfdf701961713cc57c8c9f5019853ddb500806b
pranav-8660 <pranavcse2003@gmail.com>
2026-02-08 15:05:01 +0000

    starter files

---
+++ naive-e-commerce/src/main/java/com/pranavv51/naive_e_commerce/NaiveECommerceApplication.java
@@ -0,0 +1,13 @@
+package com.pranavv51.naive_e_commerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NaiveECommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NaiveECommerceApplication.class, args);
+	}
+
+}
---
+++ naive-e-commerce/src/test/java/com/pranavv51/naive_e_commerce/NaiveECommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pranavv51.naive_e_commerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NaiveECommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
